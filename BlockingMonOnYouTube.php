1423b05212e47d32d4be32ea923235e777eab974
Smaley Company <114741665+KiselevMaksim@users.noreply.github.com>
2023-04-16 10:02:15 +0300

    Add files via upload

---
+++ BlockingMonOnYouTube.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Обучение HTML | obychalkapc.ru</title>
+</head>
+<body>
+<table border="1" width="100%" bgcolor="38728f">
+	<tr>
+		<td colspan="2">
+		<img = src="imgs/fotoshapka.jpg" width = "1304"  height="189" alt="ШАПКА САЙТА"/>
+		</td>
+	</tr>
+
+	<tr>  
+		<td width="20%">
+			<?php
+			require_once "menu.html";
+			?>
+		</td>
+		<td width="80%">
+			<div align="left"><h1><i><u>БЛОКИРОВКА МОНЕТИЗАЦИИ YouTube В РОССИИ</u></i></h1></div>
+			<p><b>КТО ДВИНЕТСЯ, ТОТ ГЕЙ; КТОТ ПРОЧИТАЛ ТОТ ГЕЙ!.</b></p>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<div align="center"><p> |Smaley company|   COPYRIGHT &copy; All rights reserved</p></div>
+		</td>
+	</tr>	
+</table>
+
+
+</body>
+</html>
---
+++ factesandnews.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Факты и новости | obychalkapc.ru</title>
+</head>
+<body>
+<table border="1" width="100%" bgcolor="38728f">
+	<tr>
+		<td colspan="2">
+		<img = src="imgs/fotoshapka.jpg" width = "1304"  height="189" alt="ШАПКА САЙТА"/>
+		</td>
+	</tr>
+
+	<tr>  
+		<td width="20%">
+			<?php
+			require_once "menu.html";
+			?>
+		</td>
+		<td width="80%">
+			<div align="center"><h1><i><u>ФАКТЫ И НОВОСТИ</u></i></h1></div>
+			<p><b>Вы находитесь на странице интересных фактов и новостей. Нажмите на ссылки в меню, чтобы переместиться.</b></p>
+			<br><br><br>
+			<p><b>СОДЕРЖИМОЕ ЭТОЙ СТРАНИЦЫ <u>ЧАСТО ИЗМЕНЯЕТСЯ!</u></b></p>
+		</td>
+		<td>
+			<div align='center'>ФАКТ <i>ТОП 1</i> <b><u>Блокировка Youtube.</u></b><div>
+			<p>Блокировки Youtube не может быть по нескольким причинам:</p>
+			<ul>
+				<li>Во-первых, данная платформа является основной платформой мира по загрузке видео;</li>
+				<li>Во-вторых, Youtube уже ввела санкции против россиян. <a href="http://obychalkapc.ru/BlockingMonOnYouTube.php">Что за санкции?</a></li>
+			</ul>
+
+
+		</td>
+
+	</tr>	
+	<tr>
+		<td colspan="2">
+			<div align="center"><p>|Smaley company|   COPYRIGHT &copy; All rights reserved</p></div>
+		</td>
+	</tr>
+</table>
+
+
+</body>
+</html>
