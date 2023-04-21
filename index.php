eca10af7f519f470460c482796161ee5d3ff550e
Smaley Company <114741665+KiselevMaksim@users.noreply.github.com>
2023-04-21 19:22:06 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -16,21 +16,9 @@
 
 	<tr>  
 		<td width="20%">
-			<div align="top"><p><u>СОДЕРЖАНИЕ САЙТА</u></p>
-			<ul>
-				<li><a href="index.html"><b>ГЛАВНАЯ</b></a></li>
-				<li><a href="python.html"><i>Обучение Python</i></a></li>
-				<li><a href="factesandnews.html"><i>Интересные факты</i></a></li>
-				<li><a href="html.html"><i>Обучение HTML</i></a></li>
-				<li><a href="help.html"><i>Помощь в обучении</i></a></li>
-			</ul></div>
-			<br>
-			<br>
-			<br>
-			<ul>
-				<li><a href="https://www.youtube.com/channel/UCP1xvdSNBzb9KLzfIpp-UKw" target="_blank"><i>Открыть YouTube канал с уроками бесплатно</i></a></li>
-
-			</ul>
+			<?php
+				require_once "menu.php";
+			?>
 		</td>
 		<td width="80%">
 			<div align="center"><h1><i><u>ГЛАВНАЯ</u></i></h1></div>
