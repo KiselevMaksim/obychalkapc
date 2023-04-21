ebaa0456182e759339e17e2b23b88e96d86d21d1
Smaley Company <114741665+KiselevMaksim@users.noreply.github.com>
2023-04-21 19:20:50 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-<div align="top"><p><u>СОДЕРЖАНИЕ САЙТА</u></p>
+			<div align="top"><p><u>СОДЕРЖАНИЕ САЙТА</u></p>
 			<ul>
 				<li><a href="index.html"><b>ГЛАВНАЯ</b></a></li>
 				<li><a href="python.html"><i>Обучение Python</i></a></li>
@@ -6,3 +6,10 @@
 				<li><a href="html.html"><i>Обучение HTML</i></a></li>
 				<li><a href="help.html"><i>Помощь в обучении</i></a></li>
 			</ul></div>
+			<br>
+			<br>
+			<br>
+			<ul>
+				<li><a href="https://www.youtube.com/channel/UCP1xvdSNBzb9KLzfIpp-UKw" target="_blank"><i>Открыть YouTube канал с уроками бесплатно</i></a></li>
+
+			</ul>
