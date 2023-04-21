5e592c4b49b7400d90b3cf1bda2abeab0ec3c11d
Smaley Company <114741665+KiselevMaksim@users.noreply.github.com>
2023-04-21 19:19:08 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<div align="top"><p><u>СОДЕРЖАНИЕ САЙТА</u></p>
+			<ul>
+				<li><a href="index.html"><b>ГЛАВНАЯ</b></a></li>
+				<li><a href="python.html"><i>Обучение Python</i></a></li>
+				<li><a href="factesandnews.html"><i>Интересные факты</i></a></li>
+				<li><a href="html.html"><i>Обучение HTML</i></a></li>
+				<li><a href="help.html"><i>Помощь в обучении</i></a></li>
+			</ul></div>
